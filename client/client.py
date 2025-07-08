fd92c06f5dec0baac590046c9346ca227172ae24
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-07-09 00:26:57 +0300

    Update client.py

--- client/client.py
+++ client/client.py
@@ -10,32 +10,21 @@ from requests.auth import HTTPBasicAuth
 #PUT: curl -X PUT -H "Content-Type: application/json" -d '{"name": "This is updated item 1"}' http://127.0.0.1:5000/api/items/1
 #DELETE: curl -X DELETE http://127.0.0.1:5000/api/items/1
 
-server_address_full = 'http://127.0.0.1:5000/tickets/'
+server_address_full = 'http://127.0.0.1:5000/tickets'
 
-def main():
-
-    print("For server->get_tickets()")
+def get_req():
+    #private_url_response = requests.get( url="https://api.github.com/user", auth=HTTPBasicAuth("username", "token") )
+    print("For server->get_tickets() request")
     try:
         print()
-        session = requests.Session()
-        session.allow_redirects = True
-        session.max_redirects = 3
-        response = session.get(server_address_full + '1')
+        response = requests.get(server_address_full + '/1', timeout=5)
         response.raise_for_status()
-        print()
-        response1 = requests.get(server_address_full + '1', timeout=5)
-        response1.raise_for_status()
         #pdb.set_trace()
         print()
         pprint.pprint(response.json())
         print(response.text)
         print(response.json())
         print(response.status_code)
-        print()
-        pprint.pprint(response1.json())
-        print(response1.text)
-        print(response1.json())
-        print(response1.status_code)
         print()
     # If the request fails (404) then print the error.
     except requests.exceptions.HTTPError as error:
@@ -49,20 +38,53 @@ def main():
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
     print()
-    #private_url_response = requests.get( url="https://api.github.com/user", auth=HTTPBasicAuth("username", "token") )
+    pass
 
+def get_sess_req():
+    print("For server->get_tickets() wih session")
+    try:
+        print()
+        session = requests.Session()
+        session.allow_redirects = True
+        session.max_redirects = 3
+        response = session.get(server_address_full + '/1')
+        response.raise_for_status()
+        print()
+        pprint.pprint(response.json())
+        print(response.text)
+        print(response.json())
+        print(response.status_code)
+        print()
+    # If the request fails (404) then print the error.
+    except requests.exceptions.HTTPError as error:
+        print(error)
+    except requests.exceptions.TooManyRedirects as error:
+        print(error)
+    except requests.ConnectionError as error:
+        print(error)
+    except requests.Timeout as error:
+        print(error)
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+    print()
+    pass
+
+def post_req():
+    #curl -X POST -H "Content-Type: application/json" -d '{ "id": 8, "title": "EEEEE" }' http://127.0.0.1:5000/tickets
     print("For server->add_ticket()")
     list1 = [8, 5, 10, 4, 5, 6, 12, 14, 16]
     id = random.choice(list1)
     print(id)
     # Composing a payload for API
+    data = { "id": 8, "title": "EEEEE" }
     payload = { "id": id, "title": "EEEEE" }
     # Defining content type for our payload
     header = { 'Content-type': 'application/json' }
     try:
         # Sending a post request to the server (API)
         #response = requests.post(url=server_address_full, data=json.dumps(payload), headers=header)
-        response = requests.post(server_address_full, json=payload, headers=header)
+        response = requests.post(server_address_full, json=data)
+        #response = requests.post(server_address_full, json=payload, headers=header)
         # Printing out the response of API
         print(response.text)
         print(response.json())
@@ -78,6 +100,12 @@ def main():
         print(error)
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
+    pass
+
+def main():
+    get_req()
+    get_sess_req()
+    post_req()
 
 if __name__ == "__main__":
     main()
